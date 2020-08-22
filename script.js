fb10e429b696dc646f6fd6d5672e22edb10fb73f
lhanano <lmhanano@gmail.com>
2020-08-21 17:49:03 -0700

    cleaned up JS with UI class

--- script.js
+++ script.js
@@ -7,44 +7,46 @@ class Book {
     this.author = author
     this.read = read
     }
-
-    getCover = function() {
-        return `<div class=cover>${this.title} by ${this.author}</div><div class="delete">delete</div></div>`
-        }
-    
-    addNewBook = function() {
-        myLibrary.push(this);
-        return this;
-    }
-
-    // determine read status to display on bookmark
-    getStatus = function() {
-        if (this.read == 'read') {
-            return `<div class="book"><div class="bookmark">${this.read}</div>`;   
-        } else if (this.read == 'unread') {
-            return `<div class="book"><div class="bookmark ${this.read}">${this.read}</div>`;  
-        }
-    }
-  
-    // read information of each book in library and create new div
-    // display library books array on shelf
-    displayBook = function() {
-        let template = '';
-        for (const element of myLibrary) {
-            template += `${element.getStatus()}${element.getCover()}`;
-        }
-    
-        const render = function (template, node) {
-            node.innerHTML = template;
-        }
-    
-        render(template, document.querySelector('.books'));
-    }
-
 }
 
 // Class: Handle UI Tasks
 class UI {
+    static displayBooks() {
+        myLibrary.forEach(book => UI.addBookToDiv(book))}
+    
+    static addBookToDiv(book) {
+            const mainDiv = document.querySelector('.books');
+            const newBook = document.createElement('div');
+            const newBookCover = document.createElement('div');
+            const newTitle = document.createElement('p');
+            const newAuthor = document.createElement('p');
+            const newBookmark = document.createElement('div');
+            const newDeleteButton = document.createElement('div');
+
+            newTitle.textContent = book.title;
+            newAuthor.textContent = book.author;
+            newBookmark.textContent = book.read;
+            newDeleteButton.textContent = 'delete';
+
+            newBook.classList.add('book');
+            newBookCover.classList.add('cover');
+            newDeleteButton.classList.add('delete');
+            newBookmark.classList.add('bookmark');
+            if (book.read == 'unread') newBookmark.classList.add('unread');
+            
+            newBookCover.appendChild(newTitle);
+            newBookCover.appendChild(newAuthor);
+            newBook.appendChild(newBookCover);
+            newBook.appendChild(newBookmark);
+            newBook.appendChild(newDeleteButton);
+            mainDiv.appendChild(newBook);
+        }
+
+    static addNewBook(book) {
+        myLibrary.push(book);
+        return myLibrary;
+    }
+
     static changeReadStatus(element) {
         if (element.classList.contains('bookmark')) {
             element.classList.toggle('unread');
@@ -67,11 +69,12 @@ book2 = new Book('The Book of Joy', 'Dalai Lama & Desmund Tutu', 'read');
 book3 = new Book('My Life In France', 'Julia Child', 'unread');
 book4 = new Book('Little Women', 'Louisa May Alcott', 'unread')
 
-book1.addNewBook();
-book2.addNewBook();
-book3.addNewBook();
-book4.addNewBook().displayBook();
+let books = [book1, book2, book3, book4];
+books.forEach(book => UI.addNewBook(book));
  
+// Event: Display books
+document.addEventListener('DOMContentLoaded', UI.displayBooks());
+
 // Event: Add new book to bookshelf via form
 let addForm = document.forms[0];
 addForm.addEventListener("submit", (e) => {
@@ -84,7 +87,9 @@ addForm.addEventListener("submit", (e) => {
     if (read.checked==true) status = 'read';
     else if (unread.checked==true) status = 'unread';
 
-    new Book(title, author, status).addNewBook().displayBook();
+    book = new Book(title, author, status);
+    UI.addNewBook(book);
+    UI.addBookToDiv(book);
 })
 
 // Event: change bookmark status
